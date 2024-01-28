3846f71f422aa9a6e704cab4b5c2da6780c9f143
Syahidan Arrizaldy Sidik <syahidan139@gmail.com>
2024-01-28 12:06:45 +0700

    initial commit

---
+++ test/class.test.ts
@@ -0,0 +1,12 @@
+describe('Class', () => {
+    it('can create class ', () => {
+        class Customer {
+
+        }
+        class Order {
+
+        }
+        const customer: Customer = new Customer();
+        const order = new Order();
+    });
+});
